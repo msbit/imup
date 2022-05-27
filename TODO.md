4cc084cd4276816a9d00870b5aad653fb70e12ad
Tom Sullivan <tom@msbit.com.au>
2022-05-28 02:55:34 +1000

    Update file handling
    Check for existence of already downloaded message, and use
    hardlinks if the same message ID is found in a different folder

--- TODO.md
+++ TODO.md
@@ -1,4 +1,3 @@
 ## TODO
 
-* Track messages in a map between `message_id` and output path, and if found use [`hard_link`](https://doc.rust-lang.org/std/fs/fn.hard_link.html) to avoid downloading redundant data
 * Consider using [`threadpool`](https://docs.rs/threadpool/latest/threadpool) with a worker per folder 
--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,13 @@ extern crate flate2;
 extern crate imap;
 extern crate native_tls;
 
+use std::collections::HashMap;
 use std::env;
 use std::error;
 use std::fmt;
 use std::fs;
 use std::io::Write;
+use std::path::Path;
 use std::result;
 use std::str;
 
@@ -44,6 +46,7 @@ fn main() -> result::Result<(), Box<dyn error::Error>> {
     let tls = native_tls::TlsConnector::builder().build()?;
     let client = imap::connect(("imap.gmail.com", 993), "imap.gmail.com", &tls)?;
     let mut session = client.login(username, password).map_err(|e| e.0)?;
+    let mut message_paths = HashMap::new();
 
     for item in session.list(Some("/"), Some("*"))?.iter() {
         if item.attributes().contains(&types::NameAttribute::NoSelect) {
@@ -57,20 +60,50 @@ fn main() -> result::Result<(), Box<dyn error::Error>> {
         let messages = session.search("ALL")?;
         let progress = ProgressBar::new(messages.len().try_into().unwrap());
         for seq in messages.iter() {
-            let fetch = session.fetch(seq.to_string(), "(BODY.PEEK[] ENVELOPE)")?;
-            for f in fetch.iter() {
+            for f in session.fetch(seq.to_string(), "(ENVELOPE)")?.iter() {
                 let envelope = f.envelope().ok_or(ApplicationError::MissingEnvelope)?;
-                let body = f.body().ok_or(ApplicationError::MissingBody)?;
-                let message_id = envelope
-                    .message_id
-                    .ok_or(ApplicationError::MissingMessageId)?;
+                let msg_id = String::from_utf8(
+                    envelope
+                        .message_id
+                        .ok_or(ApplicationError::MissingMessageId)?
+                        .to_vec(),
+                )?;
+                let filename = format!("output/{}/{}.gz", name, str::replace(&msg_id, "/", "_"));
+                if Path::new(&filename).exists() {
+                    continue;
+                }
 
-                let mut encoder = GzEncoder::new(Vec::new(), Compression::best());
-                encoder.write_all(body)?;
-                let compressed_bytes = encoder.finish()?;
-                let filebase = str::replace(str::from_utf8(message_id)?, "/", "_");
-                fs::write(format!("output/{}/{}.gz", name, filebase), compressed_bytes)?;
+                if message_paths.contains_key(&msg_id) {
+                    fs::hard_link(message_paths.get(&msg_id).unwrap(), filename)?;
+                    continue;
+                }
+
+                for f in session
+                    .fetch(seq.to_string(), "(BODY.PEEK[] ENVELOPE)")?
+                    .iter()
+                {
+                    let envelope = f.envelope().ok_or(ApplicationError::MissingEnvelope)?;
+                    assert!(
+                        msg_id
+                            == String::from_utf8(
+                                envelope
+                                    .message_id
+                                    .ok_or(ApplicationError::MissingMessageId)?
+                                    .to_vec()
+                            )?
+                    );
+
+                    let body = f.body().ok_or(ApplicationError::MissingBody)?;
+
+                    let mut encoder = GzEncoder::new(Vec::new(), Compression::best());
+                    encoder.write_all(body)?;
+                    let compressed_bytes = encoder.finish()?;
+                    fs::write(&filename, compressed_bytes)?;
+                }
+
+                message_paths.insert(msg_id, filename);
             }
+
             progress.inc(1);
         }
     }
